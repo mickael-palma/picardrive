b35745610e6586eed29e2c5907b928576f0599e1
Mickael Palma <mickael@spypp.com>
2012-06-17 07:39:01 +0200

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picardrive::Application.config.secret_token = 'b66bbb77f996410d3916b1fc1ff4075bbf3806e4af6fcd99a8d34698863406d91665db773db3cdbbbf745bfa6a2602cac56036b219eab1160b78998dbc495a39'
